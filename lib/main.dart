f495bdc47f800eb4276654a4c7b955306baab75b
Ghost <harish8404@yahoo.com>
2020-11-23 00:50:19 +0530

    Flatbutton added and func blank

--- lib/main.dart
+++ lib/main.dart
@@ -23,14 +23,18 @@ class AppBody extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: (){
+                print('Left button got pressed');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: (){
+                print('Right button got pressed');
+              },
               child: Image.asset('images/dice2.png'),
             ),
           ),
